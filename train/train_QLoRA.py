83759e37f31bde4b71a1be192d009bfe6e9d23ea
udaizin <iwanttostudypython21@gmail.com>
2024-09-29 03:09:25 +0000

    add: QLoRAで学習するコード追加

---
+++ train/train_QLoRA.py
@@ -0,0 +1,141 @@
+import json
+import argparse
+
+import torch
+from transformers import AutoTokenizer, AutoModelForCausalLM, BitsAndBytesConfig, TrainingArguments, Trainer
+from transformers.trainer_utils import set_seed
+from trl import DataCollatorForCompletionOnlyLM
+from peft import LoraConfig, TaskType, get_peft_model
+
+# 乱数シードの固定
+set_seed(42)
+
+TARGET_INTERLOCUTOR_ID = 'AD'
+TRAIN_DATA_PATH = f'./RealPersonaChat/data/train_data/{TARGET_INTERLOCUTOR_ID}_inner_monologue_train.json'
+TEST_DATA_PATH = f'./RealPersonaChat/data/test_data/{TARGET_INTERLOCUTOR_ID}_inner_monologue_test.json'
+MODEL_NAME = 'tokyotech-llm/Llama-3-Swallow-8B-Instruct-v0.1'
+QUANTIZATION_CONFIG = BitsAndBytesConfig(
+    load_in_4bit=True,
+    bnb_4bit_quant_type="nf4",
+    bnb_4bit_compute_dtype=torch.bfloat16,
+)
+LEARNING_RATE = 2e-5
+LEARNING_RATE_STR = "{:.0e}".format(LEARNING_RATE)
+DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu'
+
+# データセットの作成
+def make_tokenized_dataset(tokenizer, dialogues, without_inner_monologue):
+    dataset = []
+    if without_inner_monologue:
+        for dialogue in dialogues:
+            messages = []
+            for utterance in dialogue['utterances']:
+                if utterance['role'] == TARGET_INTERLOCUTOR_ID:
+                    messages.append({'role': 'assistant', 'content': utterance['content']})
+                else:
+                    messages.append({'role': 'user', 'content': utterance['content']})
+            dataset.append(messages)
+    else:
+        for dialogue in dialogues:
+            messages = []
+            for i in range(len(dialogue['inner_monologue_utterances'])):
+                if dialogue['inner_monologue_utterances'][i]['role'] == TARGET_INTERLOCUTOR_ID and dialogue['inner_monologue_utterances'][i]['action'] == '(thinking)':
+                    thinking = dialogue['inner_monologue_utterances'][i]['content']
+                    speaking = dialogue['inner_monologue_utterances'][i+1]['content']
+                    messages.append({'role': 'assistant', 'content': f'<thinking>{thinking}</thinking>{speaking}'})
+                elif dialogue['inner_monologue_utterances'][i]['role'] == TARGET_INTERLOCUTOR_ID and dialogue['inner_monologue_utterances'][i]['action'] == '(speaking)':
+                    continue
+                else:
+                    messages.append({'role': 'user', 'content': dialogue['inner_monologue_utterances'][i]['content']})
+            dataset.append(messages)
+
+    tokenized_dataset = [
+        tokenizer.apply_chat_template(dialogue) for dialogue in dataset
+    ]
+
+    return tokenized_dataset
+    
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--without_inner_monologue', action='store_true')
+    args = parser.parse_args()
+
+    if args.without_inner_monologue:
+        output_dir = f'./models/Swallow3-8B-{TARGET_INTERLOCUTOR_ID}-v4-QLoRA-{LEARNING_RATE_STR}-wo-im'
+    else:
+        output_dir = f'./models/Swallow3-8B-{TARGET_INTERLOCUTOR_ID}-v4-QLoRA-{LEARNING_RATE_STR}'
+
+    with open(TRAIN_DATA_PATH, 'r', encoding='utf-8') as f:
+        train_dialogues = json.load(f)
+    with open(TEST_DATA_PATH, 'r', encoding='utf-8') as f:
+        test_dialogues = json.load(f)
+
+    tokenizer = AutoTokenizer.from_pretrained(MODEL_NAME)
+    tokenized_train_dataset = make_tokenized_dataset(tokenizer, train_dialogues, args.without_inner_monologue)
+    tokenized_test_dataset = make_tokenized_dataset(tokenizer, test_dialogues, args.without_inner_monologue)
+
+    # モデルの読み込み
+    model = AutoModelForCausalLM.from_pretrained(
+        MODEL_NAME, 
+        quantization_config=QUANTIZATION_CONFIG,
+        torch_dtype=torch.bfloat16,
+        use_cache=False, # gradient checkpointingを有効にするために必要
+    )
+    # LoRAの設定
+    peft_config = LoraConfig(
+        r=64,  # 差分行列のランク
+        lora_alpha=64,  # LoRA層の出力のスケールを調整するハイパーパラメータ
+        lora_dropout=0.05,  # LoRA層に適用するドロップアウト
+        task_type=TaskType.CAUSAL_LM,  # LLMが解くタスクのタイプを指定
+        # LoRAで学習するモジュール
+        target_modules=[
+            "q_proj",
+            "k_proj",
+            "v_proj",
+            "o_proj",
+            "gate_proj",
+            "up_proj",
+            "down_proj",
+        ],
+    )
+    model.enable_input_require_grads()
+    model = get_peft_model(model, peft_config)
+    model.to(DEVICE)
+    model.print_trainable_parameters() # 学習可能なパラメータの数を表示
+
+    training_args = TrainingArguments(
+        output_dir=output_dir,
+        bf16=True,
+        num_train_epochs=10,
+        per_device_train_batch_size=4,
+        per_device_eval_batch_size=4,
+        gradient_accumulation_steps=4,
+        gradient_checkpointing=True,
+        learning_rate=LEARNING_RATE,
+        lr_scheduler_type="cosine",
+        warmup_ratio=0.1,
+        logging_steps=1,
+        save_strategy='no',
+        eval_strategy='epoch',
+        report_to='wandb',
+    )
+
+    data_collator = DataCollatorForCompletionOnlyLM(
+        tokenizer=tokenizer,
+        instruction_template='<|start_header_id|>user<|end_header_id|>\n\n',
+        response_template='<|start_header_id|>assistant<|end_header_id|>\n\n',
+    )
+
+    trainer = Trainer(
+        model=model,
+        args=training_args,
+        data_collator=data_collator,
+        train_dataset=tokenized_train_dataset,
+        eval_dataset=tokenized_test_dataset,
+        tokenizer=tokenizer,
+    )
+
+    trainer.train()
+
+    # モデルの保存
+    trainer.save_model()
